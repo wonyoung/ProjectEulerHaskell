e3eb279d723c590dcabcff5c80d20d748b06d8af
Wonyoung <wonyoungjjang@gmail.com>
2013-04-15 17:45:10 +0900

    solved problem 66

--- src/Problem66.hs
+++ src/Problem66.hs
@@ -2,15 +2,32 @@ module Problem66 where
 
 import qualified Problem64
 
---solve = aa 3
-solve = maximum . solutions 
-solutions d = map (\a -> (solutionPairs a, a)) $ Problem64.nonSquaresUnder d
-solutionPairs = foldr (\x (a,b) -> (a*x+b, a)) (1,0) . fractions
-solutionScan = scanr (\x (a,b) -> (a*x+b, a)) (1,0) . fractions
-fractions d = map (fst) list
-        where list 
-                | length equations == 2 = equations
-                | otherwise = init equations
-              equations = diophantineEquation d
-diophantineEquation d = Problem64.sqrtAsContinuedFractions d
+solve :: Integer -> Integer
+solve = snd . maximum . solutions
+ 
+solutions :: Integer -> [((Integer, Integer), Integer)]
+solutions d = map (\a -> (minPairs a, a)) $ Problem64.nonSquaresUnder d
 
+minPairs :: Integer -> (Integer, Integer)
+minPairs d = head $ filter (\(x,y) -> equationMatches x y d) $ continuedFractionPairs d
+
+equationMatches :: (Eq a, Num a) => a -> a -> a -> Bool
+equationMatches x y d =  x*x-d*y*y == 1
+
+continuedFractionPairs :: Integer -> [(Integer, Integer)]
+continuedFractionPairs = map calcPair . fractions
+
+calcPair :: [Integer] -> (Integer, Integer)
+calcPair = foldr (\x (a,b) -> (a*x+b, a)) (0,1)
+
+fractions :: Integral a => a -> [[a]]
+fractions d = [nthFraction d a | a<-[0..]]
+
+nthFraction :: Integral a => a -> Int -> [a]
+nthFraction d n = firstFraction ++ take fractionLength (cycle repeats)
+        where fractionLength = (length repeats)*n
+              repeats = tail firstFraction
+              firstFraction = fraction d
+
+fraction :: Integral b => b -> [b]
+fraction = map (fst) . Problem64.sqrtAsContinuedFractions
